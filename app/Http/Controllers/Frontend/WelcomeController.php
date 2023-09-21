e27ae152ac2a69780ba179163b4d8c2bd7d2a064
mmustafamsm <mustafamsm8@gmail.com>
2023-09-21 19:55:08 +0300

    welcome page

--- app/Http/Controllers/Frontend/WelcomeController.php
+++ app/Http/Controllers/Frontend/WelcomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontend;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Episode;
 use App\Models\Movie;
 use App\Models\TvShow;
 
@@ -12,9 +13,13 @@ class WelcomeController extends Controller
 {
     public function index()
     {
+        $movies = Movie::with('genres')->orderBy('updated_at','desc')->take(12)->get();
+        $series=TvShow::withCount('seasons')->orderBy('created_at','desc')->take(12)->get();
+        $episodes=Episode::with('season')->orderBy('created_at','desc')->take(12)->get();
         return Inertia::render('Welcome',[
-            'movies'=>Movie::with('genres')->get(),
-            'tvShows'=>TvShow::all(),
+            'movies'=>$movies,
+            'tvShows'=>$series,
+            'episodes'=>$episodes
 
             
         ]);
