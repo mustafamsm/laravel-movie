5dfa19d005b70dfb1f35e70d1b6f49bee31ab7d2
mmustafamsm <mustafamsm8@gmail.com>
2023-09-23 16:05:41 +0300

    genres index

--- app/Http/Controllers/Frontend/FrontendGenreController.php
+++ app/Http/Controllers/Frontend/FrontendGenreController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers\Frontend;
 
-use App\Http\Controllers\Controller;
+use Inertia\Inertia;
 use App\Models\Genre;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class FrontendGenreController extends Controller
 {
     public function show(Genre $genre){
-       return   $genre;
+        $movies = $genre->movies()->paginate(12);
+
+        return Inertia::render('Frontend/Genres/Index', [
+            'genre' => $genre,
+            'movies' => $movies
+        ]);
     }
 }
